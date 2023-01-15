1c3e601dfca807c82d8626d1b9085e063f614be0
dsmoljan <dorian.smoljan@croz.net>
2023-01-15 13:44:57 +0100

    Checkpoint before css refactor

--- APUW lab2/client/client.js
+++ APUW lab2/client/client.js
@@ -5,6 +5,12 @@ const serverUrl = 'http://localhost:5000';
 
 var protocol = "poll";
 
+var MSG_CHECK_INTERVAL = 60000;
+
+var pollDaemonId = null;
+
+var allMessages = [];
+
 for (let i = 0; i < radios.length; i++) {
     radios[i].addEventListener('change', function() {
         if (this.checked) {
@@ -16,15 +22,68 @@ for (let i = 0; i < radios.length; i++) {
 function selectedOptionChanged(option) {
     console.log(`Selected option changed to ${option}`);
     protocol = option;
+    registerClient();
+    initiateMessageService();
 }
+
+function initiateMessageService() {
+    if (protocol === "poll"){
+        pollDaemonId = setInterval(chkMsgPoll, MSG_CHECK_INTERVAL);
+    }else{
+        clearInterval(pollDaemonId);
+    }
+
+}
+
+function chkMsgPoll(){
+    console.log("Checking for new messages by poll protocol");
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', serverUrl + '/message/get');
+    xhr.onload = function() {
+        // a server bi mogao npr. vratiti 204 ako nema novih poruka
+        if (this.status === 200) {
+            console.log("Found a new message");
+            addAndDisplayNewRecievedMessage(JSON.parse(this.responseText).message);
+            //document.getElementById('messages').innerText = JSON.parse(this.responseText).message;
+        }
+    };
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({ from: clientId, protocol: protocol }));
+
+}
+
 
 
 function sendMessage(message) {
     console.log(`Sending message ${message}`)
+    if (protocol === "poll"){
+        sendMessagePoll(message);
+    }else if (protocol === "longpoll"){
+        sendMessageLongPoll(message);
+    }else{
+        sendMessageWebSocket(message);
+    }
 }
+
+function sendMessagePoll(message){
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", serverUrl + "/message/send", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({ from: clientId, protocol: protocol, message: message }));
+}
+
+function sendMessageLongPoll(message){
+}
+
+function sendMessageWebSocket(message){
+}
+
 
 window.addEventListener('load', function() {
     registerClient();
+    listenForMessages();
+    initiateMessageService();
 });
 
 function registerClient(){
@@ -33,11 +92,41 @@ function registerClient(){
     xhr.open("POST", serverUrl + "/register", true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onload = function() {
-        if (this.status == 200){
+        if (this.status === 200){
             console.log("Successfully registered!");
             console.log(this.response);
         }
     }
-    xhr.send(JSON.stringify({ from: clientId, message }));
+    xhr.send(JSON.stringify({ from: clientId, protocol: protocol }));
+}
 
+function listenForMessages() {
+}
+
+function addAndDisplayNewSentMessage(){
+
+}
+
+function addAndDisplayNewRecievedMessage(message){
+    let msgFromUser;
+
+    if (clientId === "A"){
+        msgFromUser = "B";
+    }else{
+        msgFromUser = "A";
+    }
+
+    allMessages.push("[" + msgFromUser + "]:" + message);
+    displayMessages();
+
+}
+
+function displayMessages(){
+    let messagesDiv = document.getElementById("messages");
+    messagesDiv.innerHTML = ""; // clear the messages div
+    for (let i = 0; i < allMessages.length; i++){
+        let messageNode = document.createElement("div");
+        messageNode.innerHTML = allMessages[i];
+        messagesDiv.appendChild(messageNode);
+    }
 }
--- APUW lab2/server/server.js
+++ APUW lab2/server/server.js
@@ -2,18 +2,76 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 
-const port = 5000
+const port = 5000;
 
-app.use(cors())
-app.use(express.json())
+var messagesForA = ["poruka"];
+var messagesForB = [];
+
+var clientAProtocol = "none";
+var clientBProtocol = "none";
+
+app.use(cors());
+app.use(express.json());
 
 // ovo samo kaze "kad dobijes get zahtjev na /, sto napraviti
 app.post('/register', (req, res) => {
     console.log("Received a new registration request")
     console.log(req.body);
+
+    if (req.body.from === 'A'){
+        clientAProtocol = req.body.protocol;
+    }else{
+        clientBProtocol = req.body.protocol;
+    }
+
     res.set('Access-Control-Allow-Origin', '*');
     res.send('Hello World!');
 })
+
+// ovo je za slucaj xmlHttpRequesta
+// ali hej sad mi je palo na pamet što brani serveru da sluša na sve moguće načine hmm
+app.post('/message/send', (req, res) => {
+    console.log("Received a new message")
+    console.log(req.body);
+
+    if (req.body.from === 'A'){
+        messagesForB.push(req.body.message);
+    }else{
+        messagesForA.push(req.body.message);
+    }
+
+    console.log(messagesForB);
+})
+
+app.post('/message/get', (req, res) => {
+    console.log("Received a new message get request")
+    console.log(req.body);
+    let body = null;
+
+    console.log(messagesForA);
+
+    if (req.body.from === 'A') {
+        if (messagesForA.length === 0){
+            res.status(204);
+            res.send();
+        }else{
+            res.status(200);
+            body = JSON.stringify({message: messagesForA.pop()});
+            res.setHeader("Content-Type", "application/json");
+            res.send(body);
+        }
+    } else {
+        if (messagesForB.length === 0){
+            res.status(204);
+            res.send();
+        }else{
+            res.status(200);
+            body = JSON.stringify({message: messagesForB.pop()});
+            res.send(body);
+        }
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
